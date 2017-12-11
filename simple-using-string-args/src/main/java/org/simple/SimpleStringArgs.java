3bd8cc47c5ab2a0afd8794848f57564f86ad12fd
Eric Everman <eeverman@gmail.com>
2017-12-11 11:27:42 -0600

    updated example

--- simple-using-string-args/src/main/java/org/simple/SimpleStringArgs.java
+++ simple-using-string-args/src/main/java/org/simple/SimpleStringArgs.java
@@ -8,11 +8,11 @@ public class SimpleStringArgs {
 	
 	final static IntProp COUNT_DOWN_START = IntProp.builder().mustBeNonNull()
 			.desc("Start the countdown from this number")
-			.mustBeGreaterThanOrEqualTo(1).defaultValue(3).build();
+			.mustBeGreaterThanOrEqualTo(1).build();
 	
 	private final static StrProp LAUNCH_CMD = StrProp.builder().mustBeNonNull()
 			.desc("What to say when its time to launch")
-			.mustMatchRegex(".*Go.*").defaultValue("GoGoGo!").build();
+			.mustMatchRegex(".*Go.*").build();
 	
 	private final static BolProp NOTIFY_NEWS = BolProp.builder().defaultValue(true)
 			.desc("If true, post launch events to the AP news feed").build();
