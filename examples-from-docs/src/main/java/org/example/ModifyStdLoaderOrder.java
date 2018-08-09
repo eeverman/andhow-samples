87debe45b245dfb45e6d217c2036df069cb8c094
Eric Everman <eeverman@gmail.com>
2018-08-09 14:19:25 -0500

    Added some examples from the docs

---
+++ examples-from-docs/src/main/java/org/example/ModifyStdLoaderOrder.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.yarnandtail.andhow.*;
+import org.yarnandtail.andhow.load.std.*;
+
+/** This class will be found at runtime and used to initiate AndHow */
+public class ModifyStdLoaderOrder implements AndHowInit {
+	@Override public AndHowConfiguration getConfiguration() {
+		return  StdConfig.instance()
+				.setStandardLoaders(StdEnvVarLoader.class, StdJndiLoader.class);
+	}
+}
---
+++ examples-from-docs/src/main/java/org/example/SetFixedValues.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.yarnandtail.andhow.*;
+import org.yarnandtail.andhow.property.StrProp;
+
+/** This class will be found at runtime and used to initiate AndHow */
+public class SetFixedValues implements AndHowInit {
+	@Override public AndHowConfiguration getConfiguration() {
+		return  StdConfig.instance()
+				.addFixedValue(MY_PROP, "some value");
+	}
+	
+	static final StrProp MY_PROP = StrProp.builder().build();
+}
