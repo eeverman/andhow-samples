c2c68bd2b242da394fd1bf3e6e8c03d192004a04
Eric Everman <eeverman@gmail.com>
2021-09-18 10:04:13 -0500

    no message

--- 02-sales-handler/src/test/java/simple/SaleHandlerTest.java
+++ 02-sales-handler/src/test/java/simple/SaleHandlerTest.java
@@ -2,36 +2,32 @@ package simple;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.yarnandtail.andhow.AndHow;
-import org.yarnandtail.andhow.api.AppFatalException;
-import org.yarnandtail.andhow.junit5.KillAndHowBeforeEachTest;
-import org.yarnandtail.andhow.junit5.RestoreSysPropsAfterThisTest;
+import org.yarnandtail.andhow.junit5.*;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-/**
- * To keep the HelloWorld2 class as simple as possible, it just prints to Sys.out.
- * 
- * This isn't best practice - Don't worry there are lots of good test examples
- * in the other samples.
- */
-@KillAndHowBeforeEachTest
 public class SaleHandlerTest {
 	
 	@Test
-	public void verySimpleDefaultTest() throws Exception {
-
-		System.out.println("Begin 'SaleHandlerTest.verySimpleDefaultTest'");
+	public void defaultConfigTaxRateShouldBe_11Percent() throws Exception {
 		SaleHandler handler = new SaleHandler();
 
-		BigDecimal tax = new BigDecimal(".11");	//This should be the configured value
-		BigDecimal sale = new BigDecimal("10.00");
-		BigDecimal expectedTotal = sale.add( sale.multiply(tax) );
+		// Total sale should be 10.00 + (10.00 * .11)
+		assertEquals(new BigDecimal("11.10"), handler.handle(BigDecimal.TEN));
+	}
 
+	@Test @KillAndHowBeforeThisTest
+	public void verifyTaxRateAt_12Percent() throws Exception {
 
-		assertEquals(expectedTotal, handler.handle(new BigDecimal("10.00")));
+		AndHow.findConfig().addFixedValue(
+				SaleHandler.Config.TAX_RATE, new BigDecimal(".12"));
+
+		SaleHandler handler = new SaleHandler();
+
+		// Total sale should be 10.00 + (10.00 * .12)
+		assertEquals(new BigDecimal("11.20"), handler.handle(BigDecimal.TEN));
 	}
 
 	
